9eaa1d5f6a3b4373774b22dcf8d73524879cd5b7
Jamexxu <jamesmercado2209@gmail.com>
2025-09-10 20:50:34 +0800

    add signup feature

--- vanilla-to-laravel/learn-php-basics/php-session_login/index.php
+++ vanilla-to-laravel/learn-php-basics/php-session_login/index.php
@@ -1,11 +1,30 @@
+<?php
+    if (isset($_POST['login'])) {
+        header("Location: login.php");
+        exit;
+    } elseif (isset($_POST['signup'])) {
+        header("Location: signup.php");
+        exit;
+    }
+?>
+
+<?php include 'bootstap.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Session Login</title>
+    <title>Sign up or Login</title>
 </head>
 <body>
-    <span>Welcome To Session Login Page</span>
+    <div class="d-flex justify-content-center align-items-center min-vh-100">
+        <div class="container">
+            <h1 class="text-center mb-4">Welcome! Please Login or Sign Up</h1>
+            <form action="index.php" method="post" class="d-flex justify-content-center gap-3">
+                <input type="submit" value="Login" name="login" class="btn btn-secondary btn-lg">
+                <input type="submit" value="Sign Up" name="signup" class="btn btn-outline-secondary btn-lg">
+            </form>
+        </div>
+    </div>
 </body>
 </html>
---
+++ vanilla-to-laravel/learn-php-basics/php-session_login/signup.php
@@ -0,0 +1,33 @@
+<?php
+    require 'db.connection.php';
+
+    if (isset($_POST['signup'])) {
+        $username = trim($_POST['username']);
+        $password = trim($_POST['password']);
+
+        if (empty($username) || empty($password)){
+            echo "Username or Password cannot be empty";
+        } else{
+            $queryCreateUser = "INSERT INTO accounts (username, password) VALUES (?, ?)";
+            $sqlCreateUser = $connection->prepare($queryCreateUser);
+            $sqlCreateUser->execute([$username, md5($password)]);
+            echo "User created successfully. You can now <a href='login.php'>login</a>.";
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signup</title>
+</head>
+<body>
+    <form action="signup.php" method="post">
+        <input type="text" name="username" placeholder="Enter Username">
+        <input type="password" name="password" placeholder="Enter Password">
+        <input type="submit" name="signup" value="Sign Up">
+    </form>
+</body>
+</html>
